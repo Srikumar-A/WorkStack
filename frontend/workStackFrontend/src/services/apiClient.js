d12991e874be361c59011fa5b52f4ed82f0e4d94
Srikumar-A <arivlagansrikumar@gmail.com>
2026-01-06 20:07:43 +0530

    deploying frontend, added backend url for now

--- frontend/workStackFrontend/src/services/apiClient.js
+++ frontend/workStackFrontend/src/services/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const apiClient=axios.create(
     {
-        baseURL:"http://127.0.0.1:8000/",
+        baseURL:import.meta.env.VITE_BACKEND_ENDPOINT,
         headers: {
         "Content-Type": "application/json",
   },
